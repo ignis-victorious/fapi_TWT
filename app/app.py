1537d8a52f3869e36fcdffef09c8aec8e24acea0
Emmanuele Levi <ignisvictorious@gmail.com>
2025-10-31 14:36:08 +0000

    add GET, both path and query parameters, HTTPExceptions, required/non-req fields

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 #
 #  Import LIBRARIES
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 #  Import FILES
+from .data.posts_dic import text_posts
+
 #  #
 
 app: FastAPI = FastAPI()
@@ -13,9 +15,23 @@ def root() -> dict[str, str]:
     return {"message": "Hello Mum"}
 
 
-@app.get(path="/hello-world")
-def hello_world() -> dict[str, str]:
-    return {"message": "Hello World"}
+@app.get(path="/posts")
+def get_all_posts(limit: int | None = None) -> list[dict[str, str]] | dict[int, dict[str, str]]:
+    if limit:
+        return list(text_posts.values())[:limit]
+    return text_posts
+
+
+@app.get(path="/posts/{id}")
+def get_post(id: int) -> dict[str, str] | None:
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found!")
+    return text_posts.get(id)
+
+
+# @app.get(path="/hello-world")
+# def hello_world() -> dict[str, str]:
+#     return {"message": "Hello World"}
 
 
 #
---
+++ app/data/posts_dic.py
@@ -0,0 +1,22 @@
+#  Import LIBRARIES
+#  Import FILES
+#  #
+# if __name__ == "__main__":
+#     main()
+
+
+text_posts: dict[int, dict[str, str]] = {
+    1: {"titLe": "New Post", "content": "cool test post"},
+    2: {"title": "Python Tip", "content": "Use list comprehensions for cleaner Loops. "},
+    3: {"title": "Daily Motivation", "content": "Consistency beats intensity every time."},
+    4: {"title": "Fun Fact", "content": "The first computer bug was an actual moth found in a Harvard Mark II. "},
+    5: {"title": "Update", "content": "Just launched my new project! Excited to share more soon. "},
+    6: {"title": "Tech Insight", "content": "Async IO in Python can massively speed up I/0-bound tasks."},
+    7: {
+        "title": "Quote",
+        "content": "`Programs must be written for people to read, and only incidentally for machine to execute` - Have",
+    },
+    8: {"title": "Weekend Plans", "content": "Might finally clean up my GitHub repos... or just play some Minecraft."},
+    9: {"title": "Question", "content": "What's the most underrated Python library you've ever used?"},
+    10: {"title": "Mini Announcement", "content": "New video drops tomorrow-covering the weirdest Python features!"},
+}
